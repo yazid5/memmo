4965e502c7b8c5dfbbb22d5e4f9300ac328643fe
OpenShift guest <infographicsports@gmail.com>
2013-09-17 07:55:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Memoo::Application.config.secret_key_base = 'ff961352a613d38a2fbf5187667492ab3fa4cc84e5e2326661d66afdbf57b6f75fbbc63e86a953b18210feb0d257da0a94357aadf989e86dedea170d750e23a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Memoo::Application.config.session_store :cookie_store, key: '_memoo_session'
